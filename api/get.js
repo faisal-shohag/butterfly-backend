767932fecf6c7d3779f526e11cbb70e8d888cd10
faisal-shohag <faisalshohagprog@gmail.com>
2024-09-24 10:04:18 +0600

    prisma issue solved

--- api/get.js
+++ api/get.js
@@ -2,7 +2,10 @@ import { Router } from 'express'
 import prisma from '../db/db.config.js'
 const router = Router()
 
-
+router.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
 
 
 
