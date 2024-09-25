53a39a58c1948c213c8b3f9d0c1b0ea318d0850f
faisal-shohag <faisalshohagprog@gmail.com>
2024-09-25 15:32:23 +0600

    updated

--- api/get.js
+++ api/get.js
@@ -1,79 +1,93 @@
-import { Router } from 'express'
-import prisma from '../db/db.config.js'
-const router = Router()
+import { Router } from "express";
+import prisma from "../db/db.config.js";
+const router = Router();
 
-router.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany()
-    res.json(users)
-})
+router.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 //get single user
-router.get('/users/:email', async(req, res)=>{
-    const email = req.params.email;
+router.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
     const user = await prisma.user.findUnique({
-        where : {
-            email
-        }
-    })
+      where: { id },
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-    return res.json({status: 200, data:user})
-})
-
+//get books
+router.get("/books", async (req, res) => {
+   try {
+     const books = await prisma.book.findMany();
+     return res.status(200).json(books);
+   } catch (error) {
+     console.log(error);
+     return res.status(500).json({ error: "Internal server error" });
+   }
+});
 
 //get all posts
-router.get('/posts', async(req, res)=>{
-    const posts = await prisma.post.findMany()
-    return res.json(posts)
-})
+router.get("/posts", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  return res.json(posts);
+});
 
 //get single post
-router.get('/posts/:authorId', async(req, res)=>{
-    const authorId = parseInt(req.params.authorId);
+router.get("/posts/:authorId", async (req, res) => {
+  const authorId = parseInt(req.params.authorId);
 
-    try {
-        const post = await prisma.post.findMany({
-            where : {
-                authorId
-            },
-            include : {
-                author : true,
-                comments : {
-                    select : {
-                        id : true,
+  try {
+    const post = await prisma.post.findMany({
+      where: {
+        authorId,
+      },
+      include: {
+        author: true,
+        comments: {
+          select: {
+            id: true,
+          },
+        },
+        likes: {
+          select: {
+            id: true,
+            userId: true,
+          },
+        },
+      },
+    });
 
-                    },
-
-                },
-                likes : {
-                    select : {
-                        id: true,
-                        userId : true
-                    }
-                }
-            }
-        })
-
-        return res.json({status: 200, data: post})
-    } catch (error) {
-        return res.status(400).json({error : error.message})
-    }
-})
-
+    return res.json({ status: 200, data: post });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
 
 //get single post comments
-router.get('/comment/:postId', async(req, res)=>{
-    const postId = parseInt(req.params.postId)
+router.get("/comment/:postId", async (req, res) => {
+  const postId = parseInt(req.params.postId);
 
-    try {
-        const comments = await prisma.comment.findMany({
-            where : {
-                postId
-            }
-        })
-        return res.json({status: 200, data: comments})
-    } catch (error) {
-        return res.status(400).json({error: error.message})
-    }
-})
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId,
+      },
+    });
+    return res.json({ status: 200, data: comments });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
 
-export default router
+export default router;
---
+++ prisma/migrations/20240925082919_made_user_id_to_string/migration.sql
@@ -0,0 +1,57 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Book" DROP CONSTRAINT "Book_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reply" DROP CONSTRAINT "Reply_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Comment" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Like" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Post" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Reply" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reply" ADD CONSTRAINT "Reply_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
