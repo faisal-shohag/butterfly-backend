54cc42ea58daed0dc375cb2eaeb5282e243f4ccd
Anas Ahammad <anasahammad2002@gmail.com>
2024-09-27 12:06:10 +0600

    stash

--- api/post.js
+++ api/post.js
@@ -3,7 +3,6 @@ import prisma from '../db/db.config.js'
 const router = Router()
 
 router.post('/users', async (req, res) => {
-    
     const user = await prisma.user.create({
         data: req.body
     })
